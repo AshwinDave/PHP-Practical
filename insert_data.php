1a74f5f54fa9d4742b26c89da266729bab37bffd
Ashwin Dave <52596969+AshwinDave@users.noreply.github.com>
2021-01-03 20:54:51 +0530

    Update insert_data.php

--- insert_data.php
+++ insert_data.php
@@ -15,11 +15,15 @@ if(isset($_POST['booking_submit'])){
 	$user_date   = mysqli_real_escape_string($conn, $_POST['user_date']);
 
 	$query = "select * from `user_booking_master` where user_email='$user_email' AND user_days='$user_days' AND user_time='$user_time' AND user_date='$user_date'";
-	echo $query;exit;
+	
 	$result = mysqli_query($conn, $query);
-	if(mysqli_num_rows($result) == 1){
-		echo "<script> alert('You Have already booking'); return false;</script>";
+	
+	if(mysqli_num_rows($result) == true){
+
+		echo "<script> alert('You Have already booking');</script>";
+		return false;
 	}else{
+		
 		$insert_query = "INSERT INTO `user_booking_master`(`user_id`, `user_name`, `user_number`, `user_email`, `user_days`, `user_time`, `user_date`, `inserted_date`) VALUES (NULL,'$user_name','$user_number','$user_email','$user_days','$user_time','$user_date',now())";
 		$insert_result = mysqli_query($conn, $insert_query);
 
