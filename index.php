4d0a8bab271f088be613e26903b32c0aab0f4bec
Ashwin Dave <52596969+AshwinDave@users.noreply.github.com>
2021-01-03 18:57:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+	/* DATABASE Connection*/
+	include "config/db_conf.php";
+	include "config/db_functions.php";
+	$conn=db_connect();
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -8,6 +14,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 		<link type="text/css" rel="stylesheet" href="style.css" />
 	</head>
 	<body>
@@ -37,39 +44,34 @@
 										<div class="col-sm-4">
 											<div class="form-group">
 												<span class="form-label">Select Days</span>
-												<select class="form-control">
-													<option value="All">All</option>
-													<option value="Monday">Monday</option>
-													<option value="Tuesday">Tuesday</option>
-													<option value="Wednesday">Wednesday</option>
-													<option value="Thuresday">Thuresday</option>
-													<option value="Friday">Friday</option>
-													<option value="Saturday">Saturday</option>
-													<option value="Sunday">Sunday</option>
+												<select id="days" class="form-control">
+													<option value="None">-- Select Days --</option>
+												<?php
+													$query = "select * from booking_days";
+													$result = mysqli_query($conn, $query);
+													if(mysqli_num_rows($result) > 0){
+														while($rows = mysqli_fetch_assoc($result)){
+												?>
+														<option value="<?php echo $rows['days_id']; ?>"><?php echo $rows['days']; ?></option>
+												<?php } } ?>
 												</select>
 												<span class="select-arrow"></span>
 											</div>
 										</div>
+
 										<div class="col-sm-4">
 											<div class="form-group">
 												<span class="form-label">Select Time Slot </span>
 												<select class="form-control">
-													<option value="10:00 AM - 10:30 AM">10:00 AM - 10:30 AM</option>
-													<option value="10:30 AM - 11:00 AM">10:30 AM - 11:00 AM</option>
-													<option value="11:00 AM - 11:30 AM">11:00 AM - 11:30 AM</option>
-													<option value="11:30 AM - 12:00 PM">11:30 AM - 12:00 PM</option>
-													<option value="12:00 PM - 12:30 PM">12:00 PM - 12:30 PM</option>
-													<option value="12:30 PM - 01:00 PM">12:30 PM - 01:00 PM</option>
-													<option value="01:00 PM - 01:30 PM">01:00 PM - 01:30 PM</option>
-													<option value="01:30 PM - 02:00 PM">01:30 PM - 02:00 PM</option>
-													<option value="02:00 PM - 02:30 PM">02:00 PM - 02:30 PM</option>
-													<option value="02:30 PM - 03:00 PM">02:30 PM - 03:00 PM</option>
-													<option value="03:00 PM - 03:30 PM">03:00 PM - 03:30 PM</option>
-													<option value="03:30 PM - 04:00 PM">03:30 PM - 04:00 PM</option>
-													<option value="04:00 PM - 04:30 PM">04:00 PM - 04:30 PM</option>
-													<option value="04:30 PM - 05:00 PM">04:30 PM - 05:00 PM</option>
-													<option value="05:00 PM - 05:30 PM">05:00 PM - 05:30 PM</option>
-													<option value="05:30 PM - 06:00 PM">05:30 PM - 06:00 PM</option>
+													<option value="None">-- Select Time Slots --</option>
+												<?php
+													$query = "select * from booking_timeslots";
+													$result = mysqli_query($conn, $query);
+													if(mysqli_num_rows($result) > 0){
+														while($rows = mysqli_fetch_assoc($result)){
+												?>
+													<option value="<?php echo $rows['timeslots_id	']; ?>"><?php echo $rows['timeslots']; ?></option>
+												<?php } } ?>
 												</select>
 												<span class="select-arrow"></span>
 											</div>
