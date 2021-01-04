959105efc7ddb2db6dbb298de5cf5e0549a92e77
Ashwin Dave <52596969+AshwinDave@users.noreply.github.com>
2021-01-04 11:38:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,23 @@
 	include "config/db_functions.php";
 	$conn=db_connect();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>Booking Form</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="images/favicon.png" type="image/gif" sizes="16x16">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
+		<script src="custom.js"></script>
 
 		<link type="text/css" rel="stylesheet" href="style.css" />
 	</head>
@@ -24,27 +31,27 @@
 					<div class="row">
 						<div class="col-md-7 col-md-push-5">
 							<div class="booking-form">
-								<form method="post" name="booking-form" action="">
+								<form method="post" name="booking-form">
 									<div class="form-group">
 										<span class="form-label">Your Name</span>
-										<input class="form-control" type="text" placeholder="Enter your name">
+										<input class="form-control" name="user_name" type="text" placeholder="Enter your name">
 									</div>
 
 									<div class="form-group">
 										<span class="form-label">Mobile Number</span>
-										<input class="form-control" type="text" placeholder="Enter your Number">
+										<input class="form-control" name="user_number" type="text" placeholder="Enter your Number">
 									</div>
 
 									<div class="form-group">
 										<span class="form-label">Email ID</span>
-										<input class="form-control" type="text" placeholder="Enter your Email ID">
+										<input class="form-control" name="user_email" type="text" placeholder="Enter your Email ID">
 									</div>
 
 									<div class="row">
 										<div class="col-sm-4">
 											<div class="form-group">
 												<span class="form-label">Select Days</span>
-												<select id="days" class="form-control">
+												<select id="days" class="form-control" name="user_days">
 													<option value="None">-- Select Days --</option>
 												<?php
 													$query = "select * from booking_days";
@@ -62,7 +69,7 @@
 										<div class="col-sm-4">
 											<div class="form-group">
 												<span class="form-label">Select Time Slot </span>
-												<select class="form-control">
+												<select class="form-control" name="user_time">
 													<option value="None">-- Select Time Slots --</option>
 												<?php
 													$query = "select * from booking_timeslots";
@@ -70,7 +77,7 @@
 													if(mysqli_num_rows($result) > 0){
 														while($rows = mysqli_fetch_assoc($result)){
 												?>
-													<option value="<?php echo $rows['timeslots_id	']; ?>"><?php echo $rows['timeslots']; ?></option>
+													<option value="<?php echo $rows['timeslots_id']; ?>"><?php echo $rows['timeslots']; ?></option>
 												<?php } } ?>
 												</select>
 												<span class="select-arrow"></span>
@@ -79,14 +86,14 @@
 										 <div class="col-sm-4">
 											<div class="form-group">
 												<div class="form-group">
-												<span class="form-label">Select Date</span>
-												<input class="form-control" type="date">
-											</div>
+													<span for="inputDate">Date</span>
+	          										<input type="input" placeholder="mm/dd/yy" name="user_date" class="form-control" id="datepicker">
+												</div>
 											</div>
 										</div>  
 									</div>
 									<div class="form-btn">
-										<button class="submit-btn">Submit Your Booking</button>
+										<input type="Submit" class="submit-btn" name="booking_submit" value="Submit Your Booking">
 									</div>
 								</form>
 							</div>
@@ -97,3 +104,36 @@
 		</div>
 	</body>
 </html>
+
+<?php 
+
+if(isset($_POST['booking_submit'])){
+
+	$user_name   = mysqli_real_escape_string($conn, $_POST['user_name']);
+	$user_number = mysqli_real_escape_string($conn, $_POST['user_number']);
+	$user_email  = mysqli_real_escape_string($conn, $_POST['user_email']);
+	$user_days   = mysqli_real_escape_string($conn, $_POST['user_days']);
+	$user_time   = mysqli_real_escape_string($conn, $_POST['user_time']);
+	$user_date   = mysqli_real_escape_string($conn, $_POST['user_date']);
+
+	$query = "select * from `user_booking_master` where user_email='$user_email' AND user_days='$user_days' AND user_time='$user_time' AND user_date='$user_date'";
+	
+	$result = mysqli_query($conn, $query);
+	
+	if(mysqli_num_rows($result) == true){
+
+		echo "<script> sweetAlert('Hi,', 'You have already booking ', 'error');</script>";
+		return false;
+	}else{
+		
+		$insert_query = "INSERT INTO `user_booking_master`(`user_id`, `user_name`, `user_number`, `user_email`, `user_days`, `user_time`, `user_date`, `inserted_date`) VALUES (NULL,'$user_name','$user_number','$user_email','$user_days','$user_time','$user_date',now())";
+		$insert_result = mysqli_query($conn, $insert_query);
+
+		if($insert_result){
+			header('location: index.php');
+		}else{
+			echo "<script> alert('Somthing Went Wrong'); </script>";
+		}
+	}
+
+}
